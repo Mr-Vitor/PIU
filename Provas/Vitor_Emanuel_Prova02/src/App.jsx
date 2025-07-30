b92252422265a71db940a65137f3417de8ce984a
Mr-Vitor <vitor.e.demedeiros@gmail.com>
2025-07-30 14:23:42 -0300

    provaPIU02

---
+++ Provas/Vitor_Emanuel_Prova02/src/App.jsx
@@ -0,0 +1,15 @@
+import Login from './componentes/Login'
+import IdRecovery from './componentes/IdRecovery'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <IdRecovery/>
+      <Login/>
+    </>
+  )
+}
+
+export default App
